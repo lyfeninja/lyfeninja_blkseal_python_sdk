2bc2eb9b9786b2b3324f3eed879f7cd1367cda0a
Travis Jones <travis.jones.610@gmail.com>
2026-05-11 12:04:25 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='blkseal',
+    version='0.1.0',
+    py_modules=['blkseal'],
+    author='Travis Jones',
+    author_email='travisjones@lyfe.ninja',
+    description='BlkSeal Python SDK for signing and verifying content',
+    long_description='Lightweight SDK for interacting with BlkSeal signing and verification APIs.',
+    long_description_content_type='text/plain',
+    url='https://github.com/lyfe-ninja/blkseal',
+    license='Apache-2.0',
+    python_requires='>=3.8',
+)
